ec2304e52dab75500a3b5a2673674997779cd6c1
Andrea_Ricci <andrea.ricci60@studio.unibo.it>
2025-11-05 23:00:02 +0100

    Implementation third part of exercises 72

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,21 +1,57 @@
 package it.unibo.nestedenum;
 
 import java.util.Comparator;
-import java.util.Locale;
-import java.util.Objects;
+
 
 /**
  * Implementation of {@link MonthSorter}.
  */
-public final class MonthSorterNested implements MonthSorter {
 
+public final class MonthSorterNested implements MonthSorter {
+    private enum Month {
+        JANUARY(31),FEBRUARY(28),MARCH(31),APRIL(30),MAY(31),JUNE(30),JULY(31),AUGUST(31),
+        SEPTEMBER(30),OCTOBER(31),NOVEMBER(30),DECEMBER(31);
+
+        private final int numDay; 
+        private Month(int numDay)
+        {
+            this.numDay = numDay;
+        }
+
+       static private boolean isTheMonth(Month m, String s){
+            return (m.name().toLowerCase().equals(s.toLowerCase()) || m.name().toLowerCase().startsWith(s.toLowerCase()));
+        }
+       static public Month fromString(String s)
+        {
+            for(Month m : Month.values()){
+                if(s.length() > 1 && isTheMonth(m, s)){
+                    return m;
+                }
+            }
+            throw new IllegalArgumentException();
+
+        }
+        public int getDays(){
+            return numDay;
+        }
+    }
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2){
+                return Integer.compare(Month.fromString(o1).getDays(), Month.fromString(o2).getDays());
+            }
+        };
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2){
+                return Integer.compare(Month.fromString(o1).ordinal(), Month.fromString(o2).ordinal());
+            }
+        };
     }
 }
